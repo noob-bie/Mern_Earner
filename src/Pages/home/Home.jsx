33e52250883fdf725ecd84d366c91a8414b7080b
Twaseen47 <140600557+Twaseen@users.noreply.github.com>
2024-06-05 09:20:18 +0600

    check

---
+++ src/Pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useReducer, useState } from "react";
+import "Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">Home</div>
+    
+  );
+};
+
+export default Add;
